712e733a21fab804abe1e8db481fd192c80a1e7f
Roman Kohutyak <romankohutyak@gmail.com>
2021-01-31 14:57:53 +0200

    fix: add routes to static

--- server/src/createApp.ts
+++ server/src/createApp.ts
@@ -10,6 +10,12 @@ import { getListsRouter } from './lists/list.router';
 export const createApp = (mongoClient: RSMongoClient) => {
   const app = express();
 
+  // If your lib folder is on any other location then specify the absolute path to the lib folder like app.use(express.static('path to lib from /');
+  app.use(express.static('/../client/dist'));
+  app.get('*', (req, res) => {
+    res.sendFile('/../client/dist/index.html');
+  });
+
   app.use(morgan('dev'));
   app.use(cors());
   app.use(bodyParser.json());
